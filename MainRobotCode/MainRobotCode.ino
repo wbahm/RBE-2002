481850003c243d76c4c6ee7f784bb1127864f61c
vargoal <vargoal@autoreg-073530.dyn.wpi.edu>
2016-10-31 00:00:27 -0400

    Both sides motor control at 16 khz works with OScope. Still a littl noise when low.

--- MainRobotCode/MainRobotCode.ino
+++ MainRobotCode/MainRobotCode.ino
@@ -1,22 +1,10 @@
 #include "DrivePWM.h"
 void setup() {
-  initDrivePWM(LEFT);
-  /*pinMode(3, OUTPUT);
-  pinMode(11, OUTPUT);
-  TCCR2A = _BV(COM2A0) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
-  TCCR2B = _BV(WGM22) | _BV(CS22);
-  OCR2A = 180;
-  OCR2B = 50;*/
+  initDrivePWM();
 }
 
 void loop() {
-  //for(int i = 0; i<30000;i++)
-  //{
-    setDrivePWM(65535,LEFT);
+    setDrivePWM(65535*0.75,LEFT);
+    setDrivePWM(65535*0.5,RIGHT);
     delay(500);
-    setDrivePWM(0,LEFT);
-    delay(500);
-  //}
-//  setDrivePWM(30000,LEFT);
- // delay(100);
 }
