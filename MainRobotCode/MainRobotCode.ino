b4760525578ceb32a34b279ef47883313107d1cb
vargoal <vargoal@autoreg-073530.dyn.wpi.edu>
2016-12-09 21:23:52 -0500

    Just copied odometry file to main robot code as it has the most working

--- MainRobotCode/MainRobotCode.ino
+++ MainRobotCode/MainRobotCode.ino
@@ -1,10 +1,359 @@
+#include <digitalIOPerformance.h>
 #include "DrivePWM.h"
+#include "WallMath.h"
+#include "PID.h"
+
+#define DEBUG_PRINT //used to enable debug print statements
+#include "debug.h"
+
+#include "Encoder.h"
+Encoder leftEnc(20,19);
+Encoder rightEnc(18,3);
+
+#include "RobotOdometry.h"
+
+unsigned long long lastContr = 0;
+const unsigned long long contrFreq = 200; //hz
+const unsigned long long contrT = 1000000/contrFreq;
+
+static const long long UM_PER_TICK = 27;
+static const long long US_TO_SEC = 1000000;
+static const long long SETPOINT = 0.75*2.75*PI*2.54*10000; // um/sec -> 3.9 in/sec -> 0.46 rps
+static const long long WALL_OFFSET_SETPOINT = (long)10*25.4;
+
+PID* leftPid;
+PID* rightPid;
+long lastLeftPwr = 0;
+long lastRightPwr = 0;
+unsigned long long lastLeftEnc = 0;
+unsigned long long lastRightEnc = 0;
+
+PID* wallOffsetPid;
+PID* wallThetaPid;
+
+PID* turnPid;
+
+typedef enum RobotState {
+  WALL_FOLLOW,
+  TURN,
+  STANDBY,
+  SPEC_CASE, //Detected that front wall sensor maxed and other isn't
+  FORWARD_DIST
+};
+
+RobotState currentState = STANDBY;
+RobotState previousState = STANDBY;
+
 void setup() {
-  initDrivePWM();
+  DebugBegin();
+  DebugPrintln("Serial started");
+  wallInit();
+  initDrivePWM();//sets up timer for PWM generation
+  leftPid = new PID(100,0,0);
+  rightPid = new PID(100,0,0);
+  wallOffsetPid = new PID(3000000,0,0);//600000
+  wallThetaPid = new PID(600000,0,0); 
+  turnPid = new PID(1184976,0,0);
 }
+unsigned long long lastWallPID = 0;
+long long lastLeftSetpoint = SETPOINT;//+50000;
+long long lastRightSetpoint = SETPOINT;//-50000;
+long long walloffsetpidOut = 0;
+long long wallthetapidOut = 0;
+long long MAX_SETPOINT = 0.9*2.75*PI*2.54*10000;
+long long MIN_SETPOINT = 0.3*2.75*PI*2.54*10000;
+void handleWallPID();
+void handleTurn();
+void handleSpecCase();
+void handleForward();
+float targetTurnHeading=0; //Rad
+long long forwardTarget = 0;//mm dist
 
 void loop() {
-    setDrivePWM(65535*0.75,LEFT);
-    setDrivePWM(65535*0.5,RIGHT);
-    delay(500);
+  switch(currentState)
+  {
+    case STANDBY://add btn later
+    currentState = WALL_FOLLOW;
+    break;
+    case SPEC_CASE:
+    handleSpecCase();
+    break;
+    case WALL_FOLLOW:
+    handleWallPID();
+    break;
+    case TURN:
+    handleTurn();
+    break;
+    case FORWARD_DIST:
+    handleForward();
+    break;
+    default:
+    1+1;
+    break;
+  }
+  handleMotorControl(lastLeftSetpoint,lastRightSetpoint);
+  computeOdometry(&leftEnc,&rightEnc);
+/*  DebugPrint(getXLoc()); //mm
+  DebugPrint('\t');
+  DebugPrint(getYLoc()); //mm
+  DebugPrint('\t');
+  DebugPrintln(getTheta()*(180/PI)); //deg*/
+}
+void handleMotorControl(long long leftSetpoint,long long rightSetpoint) {
+    if((micros()-lastContr)>contrT)
+  {  
+    //Get time diff and enc diff
+    long long diffTime = micros()-lastContr;
+    lastContr = micros();
+    long long leftDiffTicks = leftEnc.read()-lastLeftEnc;
+    lastLeftEnc = leftEnc.read();
+    long long rightDiffTicks = rightEnc.read()-lastRightEnc;
+    lastRightEnc = rightEnc.read();
+    //compute velocity
+    long long leftVel = ((leftDiffTicks*US_TO_SEC*UM_PER_TICK)/diffTime); //in mm/sec
+    long long rightVel = ((rightDiffTicks*US_TO_SEC*UM_PER_TICK)/diffTime); //in mm/sec
+    //compute PID
+    long long leftpidOut = leftPid -> compute(leftSetpoint,leftVel);
+    long long rightpidOut = rightPid -> compute(rightSetpoint,rightVel);
+    //set output
+    lastLeftPwr += leftpidOut;
+    lastLeftPwr = constrain(lastLeftPwr,-65535,65535);
+    lastRightPwr += rightpidOut;
+    lastRightPwr = constrain(lastRightPwr,-65535,65535);
+    if(lastLeftPwr > 0)
+    {
+      setDrivePWM((unsigned int)lastLeftPwr, LEFT,FORWARD);
+    }
+    else
+    {
+      setDrivePWM((unsigned int)(-1*lastLeftPwr), LEFT,BACKWARD);
+    }
+    if(lastRightPwr > 0)
+    {
+      setDrivePWM((unsigned int)lastRightPwr, RIGHT,FORWARD);
+    }
+    else
+    {
+      setDrivePWM((unsigned int)(-1*lastRightPwr), RIGHT,BACKWARD);
+    }
+  }
+}
+
+void handleWallPID() {
+  static long long stoppedTime = 0;
+  static bool doneTurn = false;
+  if((micros()-lastWallPID) >(16667)) //60 hz
+  {
+    lastWallPID = micros();
+    WallState newState = getWallState(RIGHT_WALL);
+    DebugPrintln((newState.frontDist < (25.4*10))&&(newState.frontDist>5));
+    if((newState.frontDist < (25.4*10))&&(newState.frontDist>5))
+    {
+      lastLeftSetpoint = 0; //stop if wall in front is too close
+      lastRightSetpoint = 0;
+      if(doneTurn == false)
+      {
+        doneTurn = true;
+        stoppedTime = micros();
+      }
+      targetTurnHeading = getTheta()+(PI/2.0);
+      if((micros()-stoppedTime)>500000)
+      {
+        doneTurn = false;
+        currentState = TURN;
+        previousState = WALL_FOLLOW;
+        DebugPrintln("Back to turn");
+      }
+    }
+    else if(newState.specCase)
+    {
+      currentState = SPEC_CASE;
+    }
+    else
+    {
+      walloffsetpidOut = wallOffsetPid -> compute(WALL_OFFSET_SETPOINT,newState.wallDist);
+      wallthetapidOut = wallThetaPid -> compute(0,newState.theta);
+      
+      lastLeftSetpoint = (SETPOINT-walloffsetpidOut+wallthetapidOut);
+      lastRightSetpoint = (SETPOINT+walloffsetpidOut-wallthetapidOut);
+      lastLeftSetpoint = constrain(lastLeftSetpoint,MIN_SETPOINT,MAX_SETPOINT);
+      lastRightSetpoint = constrain(lastRightSetpoint,MIN_SETPOINT,MAX_SETPOINT);
+      
+      /*DebugPrint(newState.wallDist/25.4);
+      DebugPrint('\t');
+      DebugPrint((newState.theta/PI)*0.180);
+      DebugPrint('\t');
+      DebugPrint('\t');
+      DebugPrint((long)walloffsetpidOut);
+      DebugPrint('\t');
+      DebugPrint((long)wallthetapidOut);
+      DebugPrint('\t');
+      DebugPrint('\t');
+      DebugPrint((long)lastLeftSetpoint);
+      DebugPrint('\t');
+      DebugPrint((long)lastRightSetpoint);
+      DebugPrint('\t');
+      DebugPrint('\t');*/
+      static long lastLeftVal = leftEnc.read();
+      long leftdiff = leftEnc.read()-lastLeftVal;
+      lastLeftVal = leftEnc.read();
+      static long lastRightVal = rightEnc.read();
+      long rightdiff = rightEnc.read()-lastRightVal;
+      lastRightVal = rightEnc.read();
+      /*DebugPrint((leftdiff/8245.81)*(1000000/16667.0));
+      DebugPrint('\t');
+      DebugPrintln((rightdiff/8245.81)*(1000000/16667.0));*/
+    }
+  }
+}
+
+static const float TURN_TOL = 5*(PI/180.0); //Rad
+
+void handleTurn() {
+  static long long stoppedTime = 0;
+  static bool doneTurn = false;
+  //Serial.println("Turn");
+  if(targetTurnHeading>(2*PI))
+  {
+    targetTurnHeading -= (2*PI);
+  }
+  float currentHeading = getTheta();
+  float absError = abs(currentHeading-targetTurnHeading);
+  if(absError>PI) absError = (2*PI)-absError;
+  long long pidOut = abs(turnPid -> compute(0,abs(currentHeading)*10000)); //scaled because pid uses long longs
+  if((targetTurnHeading>currentHeading) xor (abs(targetTurnHeading-currentHeading)>PI))
+  {
+    //turn counter clockwise
+    lastLeftSetpoint = constrain(-1*pidOut,-1*MAX_SETPOINT,MAX_SETPOINT);
+    lastRightSetpoint = constrain(pidOut,-1*MAX_SETPOINT,MAX_SETPOINT);
+  }
+  else
+  {
+    //turn clockwise
+    lastLeftSetpoint = constrain(pidOut,-1*MAX_SETPOINT,MAX_SETPOINT);
+    lastRightSetpoint = constrain(-1*pidOut,-1*MAX_SETPOINT,MAX_SETPOINT);
+  }
+  DebugPrint((long)lastLeftSetpoint);
+  DebugPrint('\t');
+  DebugPrint((long)lastRightSetpoint);
+  DebugPrint('\t');
+  DebugPrintln(abs(getTheta()-targetTurnHeading));
+  if(abs(getTheta()-targetTurnHeading)<TURN_TOL)
+  {
+    if(doneTurn == false)
+    {
+      doneTurn = true;
+      stoppedTime = micros();
+    }
+      lastLeftSetpoint = 0; //stop motors
+      lastRightSetpoint = 0;
+      if((micros()-stoppedTime)>500000)
+      {
+        currentState = previousState;
+        previousState = TURN;
+        doneTurn = false;
+      }
+  }
+}
+
+void handleSpecCase() {
+  static enum SpecCaseStep{
+    FORWARD_FIRST,
+    TURN_FIRST,
+    FORWARD_SECOND,
+    TURN_SECOND,
+    FORWARD_THIRD,
+    DONE
+  } myState = FORWARD_FIRST;
+  switch(myState)
+  {
+    case FORWARD_FIRST:
+    previousState = currentState;
+    //set forward target
+    forwardTarget = (25.4*8);
+    currentState = FORWARD_DIST;
+    myState = TURN_FIRST;
+    break;
+    case TURN_FIRST:
+    previousState = currentState;
+    //set turn target
+    targetTurnHeading = getTheta()-(PI/2);
+    currentState = TURN;
+    myState = FORWARD_SECOND;
+    break;
+    case FORWARD_SECOND:
+    previousState = currentState;
+    //set forward target
+    forwardTarget = (25.4*15);
+    currentState = FORWARD_DIST;
+    myState = TURN_SECOND;
+    break;
+    case TURN_SECOND:
+    previousState = currentState;
+    //set turn target
+    targetTurnHeading = getTheta()-(PI/2);
+    currentState = TURN;
+    myState = FORWARD_THIRD;
+    break;
+    case FORWARD_THIRD:
+    previousState = currentState;
+    //set forward target
+    forwardTarget = (25.4*10);
+    currentState = FORWARD_DIST;
+    myState = DONE;
+    break;
+    case DONE:
+    previousState = currentState;
+    currentState = WALL_FOLLOW;
+    myState = FORWARD_FIRST;
+    break;
+    default:
+    break;
+  }
+}
+
+static const int FORWARD_TOL = 0.5*25.4;//mm
+void handleForward() {
+  static bool started = false;
+  static long startX = 0; //mm
+  static long startY = 0; //mm
+  static long long stopTime = 0;
+  static bool stopping = false;
+  if(started == false)
+  {
+    startX = getXLoc();
+    startY = getYLoc();
+    started = true;
+  }
+  long diffX = abs(getXLoc()-startX);
+  long diffY = abs(getYLoc()-startY);
+  long distTravel = sqrt((diffX*diffX)+(diffY*diffY));
+  if((forwardTarget-distTravel)>FORWARD_TOL)
+  {
+    lastLeftSetpoint = SETPOINT;
+    lastRightSetpoint = SETPOINT;
+  }
+  else if((distTravel-forwardTarget)>FORWARD_TOL)
+  {
+    lastLeftSetpoint = -1*SETPOINT;
+    lastRightSetpoint = -1*SETPOINT;
+  }
+  else
+  {
+    //in tol
+    lastLeftSetpoint = 0;
+    lastRightSetpoint = 0;
+    if(stopping == false)
+    {
+      stopping = true;
+      stopTime = micros();
+    }
+    if((micros()-stopTime)>500000)
+    {
+      stopping = false;
+      started = false;
+      currentState = previousState;
+      previousState = FORWARD_DIST;
+    }
+  }
 }
---
+++ MainRobotCode/RobotOdometry.cpp
@@ -0,0 +1,48 @@
+#include "RobotOdometry.h"
+#include "Arduino.h"
+/*static RobotPos {
+  long x_loc = 0; //um
+  long y_loc = 0; //um
+  long theta = 0; //uRad
+  long lastLeftEnc = 0;
+  long lastRightEnc = 0;
+} myPosition;
+
+static const long long FREQ_ODOMETRY = 200; //hz
+static const long long US_PER_ODOMETRY = 1000000/FREQ_ODOMETRY; //in usec*/
+
+void computeOdometry(Encoder* leftEnc,Encoder* rightEnc) { //called as fast as possible, it handles timing
+  if((micros()-myPosition.lastOdometryTime) > US_PER_ODOMETRY) //controls update rate
+  {
+    myPosition.lastOdometryTime = micros();
+    long leftReading = leftEnc->read();
+    long rightReading = rightEnc->read(); 
+    long leftDiff = leftReading-myPosition.lastLeftEnc;
+    long rightDiff = rightReading-myPosition.lastRightEnc;
+    myPosition.lastLeftEnc = leftReading;
+    myPosition.lastRightEnc = rightReading;
+    float leftDist = leftDiff*LEFT_DIST_PER_TICK;
+    float rightDist = rightDiff*RIGHT_DIST_PER_TICK;
+    
+    float distTraveled = (leftDist+rightDist)/2.0;
+    myPosition.x_loc += sin((PI/2)-(myPosition.theta/1000.0))*distTraveled;
+    myPosition.y_loc += -1*cos((PI/2)-(myPosition.theta/1000.0))*distTraveled;
+    
+    float encDeltaTheta = ((rightDist-leftDist)/WHEEL_BASE)*1000; //uRad
+    //Serial.println(myPosition.theta);
+    myPosition.theta += encDeltaTheta; //add gyrodometry here
+    if(myPosition.theta > (2*PI*1000))
+      myPosition.theta = myPosition.theta-(2*PI*1000);
+    
+  }
+}
+
+long getXLoc() {//Returns value in mm
+  return (myPosition.x_loc/1000.0f);
+}
+long getYLoc() {//Returns value in mm
+  return (myPosition.y_loc/1000.0f);
+}
+float getTheta() {//Returns value in Rad
+  return (myPosition.theta/1000.0f);
+}
